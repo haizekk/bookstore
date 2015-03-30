f63ffde151fd9502c2d6bb296fa6e793b3bcb911
raluka <badoi.raluca@gmail.com>
2015-03-30 15:18:22 +0200

    friendly url with id and slug

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ActiveRecord::Base
+  before_save :update_slug
+
+  def update_slug
+    self.slug = name.parameterize
+  end
+
+  def to_param
+    "#{id}-#{name.parameterize}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326191019) do
+ActiveRecord::Schema.define(version: 20150330130728) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
 
 end
